e40362808cbc1cfc429c7b7b50be7273f6fe829e
Claire <cmparr.stu@ubc.ca>
2022-03-22 17:16:05 -0700

    Changed boundary function to hold condition for motion and include boundaries for motion case. Added initial conditions for both cases

--- glacier.py
+++ glacier.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 import numpy as np
 
-def glacier(ngridx, ngridz, dt, T):  # return eta
+def glacier(ngridx, ngridz, dt, zinput, T, motion = False):  # return eta
     '''recommended values ngrid=11, dt=150, T=4*3600 (4 hours)???? CHANGE FOR OUR PROJECT
+    if motion = True motion case for BCs, initial, stepper (eventually) will be used
     '''
     g = 10 
-    D = 200           #depth of our domain in x direction [m]
-    L = 20e3          #length of our domain in x direction [m]
-    C0 = 10           # input concentration of methane NOT TRUE
-    S0 = 0            # Input concentration of salinity 
-    zz = 2 
+    D = 200            # depth of our domain in x direction [m]
+    L = 20e3           # length of our domain in x direction [m]
+    C0 = 10            # input concentration of methane          NOT TRUE
+    S0 = 0             # Input concentration of salinity 
+    zz = zinput        # *** Scale so input height matches grid *** 
     dx = L/(ngridx-1)
     dz = D/(ngridz-1)
+    u0 = 0             # Input velocity of FW plume              NEEDS A REAL VALUE
   
 
 # set up temporal scale T is total run time
@@ -19,7 +21,8 @@ def glacier(ngridx, ngridz, dt, T):  # return eta
 
 # initialize
     u, w, rho, S, C = init0(ngridx,ngridz,ntime)
-    C,S=boundary_steady(C, S)
+    C, S = inital_steady(C, S)
+    C,S=boundaries(C, S, u, w, uo, C0, S0, zz, D, motion)
     return C,S
 
 
@@ -45,10 +48,10 @@ def init0(ngridx,ngridz,ntime):
     '''initialize a ngrid x ngrid domain, u, v,, all zero 
      we need density salinity, ch4''' 
 
-    u = np.zeros((ntime,ngridx, ngridz))
+    u = np.zeros((ntime,ngridx, ngridz))       ## [time, x, z]
     w = np.zeros_like(u)
-    S = np.zeros((ntime,ngridx-1, ngridz-1))
-    C = np.zeros_like(S)
+    S = np.ones((ntime,ngridx-1, ngridz-1))    ## Needs to be 1 for setting steady init conditions. If problem can set this in init func   
+    C = np.ones_like(S)
     rho = np.zeros_like(S)
 
     return  u, w, rho, S, C 
@@ -76,8 +79,8 @@ def stepgrid2(ngrid, f, g, Hu, Hv, dt, rdx, u, v, eta, up, vp, etap):
     return u, v, eta
 
 
-def boundary_steady(C, S, C0, S0):
-    '''Sets the boundary conditions for the steady state and for the source and sink stage'''
+def boundaries(C, S, u, w, uo, C0, S0, zz, D, motion):
+    '''Sets the boundary conditions for the steady state if motion = False, boundaries for motion case if true'''
     ## open water boundary
     C[-1, :] = 4.5 ## nM
     S[-1, :] = 35 ## PSU
@@ -87,5 +90,46 @@ def boundary_steady(C, S, C0, S0):
     C[0, zz] = C0
     S[0, :] = S[1,:]
     S[0, zz] = S0
+    
+    if motion:
+        ''' set BCs for velocity'''
+        ## Surface and depth boundary
+        w[:, 0] = w[:, D] = 0
+        u[:, -1] = u[:, -2]
+        u[:, 0] = u[:, 1]
+        ## Wall boundary
+        w[0, :] = u[0, :] = 0
+        u[0, zz] = u0
+        ## open boundary
+        w[-1, :] = w[-2, :]
+        u[-1, :] = w[-1, :]
+        
+    return C, S, u, w
+'''
+def boundary_motion(C, S, u, w, uo, C0, S0, zz, D):
+    
+    C, S = boundary_steady(C, S, C0, S0, zz)
+    ## Surface and depth boundary
+    w[:, 0] = w[:, D] = 0
+    u[:, -1] = u[:, -2]
+    u[:, 0] = u[:, 1]
+    ## Wall boundary
+    w[0, :] = u[0, :] = 0
+    u[0, zz] = u0
+    ## open boundary
+    w[-1, :] = w[-2, :]
+    u[-1, :] = w[-1, :]
+    
+    return C, S, u, w
+'''
 
+def initial_steady(C, S):
+    ''' sets the inital conditions for the steady state stages'''
+    C  = 4.5*C
+    S = 35*S
     return C, S
+
+def inital_motion(C, S, u, w):
+    ''' sets initial conditions for motion case '''
+    C, S = initial_steady(C, S)       ## A placeholder until we have a steady field solution. maybe use the steady state stepper
+    u, w = 0                          ## should be set when creating grids anyway
