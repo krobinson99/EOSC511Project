298465d76f7250be4757d41a0447f86cdfa36dc9
JoseEOAS <jvalenti@eoas.ubc.ca>
2022-04-01 18:44:57 -0700

    euler2d

--- euler2d.py
+++ euler2d.py
@@ -18,48 +18,56 @@ D = 200
 dx = L/N    #25km
 dz = D/M
 Cou = 0.05 #Courant number
-u0 = -0.1   #advection velocity km/s 
-dt = int(Cou*dx/abs(u0))    #timestep seconds 
+u0 = 5   #advection velocity km/s 
+dt = 2   #timestep seconds 
 C0 = 100    #Initial concentration
-tmax = dt*300  #number of timesteps in order to compute a full cycle
+tmax = 1000  #number of timesteps in order to compute a full cycle
 #Initial cond
 # itions and vectors
 x = np.arange(0,L,dx) 
 z = np.arange(0,D,dz) 
 
+u = u0*np.linspace(-1,1,int(D/dz))
 C = np.zeros([tmax,N,M])
-C[0,int(L/(5*dx)):int(L/(10*dx)),int(D/(5*dz)):int(D/(10*dz))] = C0
-
+C[0,23:27,0:D] = C0
+X,U=np.meshgrid(x,u)
 X,Z=np.meshgrid(x,z)
-print(C)
-plt.contour(X,Z,C[0,:,:].T)
-plt.show()
-
+U=U.T
+# plt.contourf(X,Z,U.T)
+# plt.show()
 #Define the scheme functions 
 #EULER FORWARD UPSTREAM SCHEME
-def upstr(c,ca):
-    cf = c - dt*(abs(u0)*(c - ca)/dx)
+def upstr(c,ca,uj):
+    cf = c - dt*(abs(uj)*(c - ca)/dx)
     return cf
 
 #LAX-WENDROFF SCHEME
-def lax(ca,c,cs):
-    cf = c - dt*(u0*(cs - ca)/(2*dx)) + dt*((u0**2)*(dt/2)*(cs -2*c + ca)/(dx**2))
+def lax(ca,c,cs,uj):
+    cf = c - dt*(uj*(cs - ca)/(2*dx)) + dt*((uj**2)*(dt/2)*(cs -2*c + ca)/(dx**2))
     return cf
 
 
 ######################################
 
 ###############  UNCOMMENT TO USE EULER SCHEME ###############
+
 # for n in range(0,tmax-1):
-#     for j in range(0,N):
-#         C[n+1,j] = upstr(C[n,j],C[n,j-1])
+#     for i in range(0,N-1):
+#         for j in range(0,M):
+#             if U[i,j]>=0:
+#                 C[n+1,i,j] = upstr(C[n,i,j],C[n,i-1,j],U[i,j])
+#             else:
+#                 C[n+1,i,j] = upstr(C[n,i,j],C[n,i+1,j],U[i,j])
 # titulo=str('Concentration advection computed with Euler Math.Scheme')
 
-###############  UNCOMMENT TO USE EULER SCHEME negative ###############
-for n in range(0,tmax-1):
-    for j in range(0,N-1):
-        C[n+1,j] = upstr(C[n,j],C[n,j+1])
-titulo=str('Concentration advection computed with Euler Math.Scheme')
+##############  UNCOMMENT TO USE LAX SCHEME  ###############
+
+# for n in range(0,tmax-1):
+#     for i in range(0,N-1):
+#         for j in range(0,M):
+#             C[n+1,i,j] = lax(C[n,i-1,j],C[n,i,j],C[n,i+1,j],U[i,j])
+        
+# titulo=str('Concentration advection computed with Lax Wendrof Math.Scheme')
 
 
 #Generate animation and play it in loop but don't save it
@@ -71,9 +79,8 @@ def animate(frames):
         
     plt.clf()
     ax = fig.add_subplot(111)
-    ax.set_ylim(-20, C0+20)
-    a = C[frames,:]
-    oo = plt.plot(X,a)
+    a = C[frames,:,:] 
+    oo = plt.contourf(X,-Z,a.T,vmin=0,vmax=100)
     plt.title(titulo)
     plt.ylabel('Concentration')
     plt.xlabel('x space (km)')
@@ -81,6 +88,6 @@ def animate(frames):
     
     return oo
 
-#ani = animation.FuncAnimation(fig, animate, frames=np.arange(0,tmax,50))
-#plt.show()
+ani = animation.FuncAnimation(fig, animate, frames=np.arange(0,tmax,10))
+plt.show()
 
